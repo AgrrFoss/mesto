6d5b721d72f880808d6e821b3403b14d73e0cc88
Gregori Orlov <orlov.gregori@yandex.ru>
2022-04-14 14:21:52 +0300

    Сделать выкладку карточек через Js, настроить лайки и удаление карточек

--- scripts/script.js
+++ scripts/script.js
@@ -36,12 +36,76 @@ function formSubmitHandler (evt) {
 }
 editForm.addEventListener('submit', formSubmitHandler);
 
-//......................... Код для открытия и закрытия попапа добавления карточки.........................
+
+//....................................Массив объектов для карточек............................................
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+//....................................Код для наполнения страницы карточками.........................
+let elements = document.querySelector('.elements');//Находим куда добавлять элементы
+let elementTemplate = document.querySelector('#elementTemplate');//Находим шаблон элемента
+let elementPlace// = elementTemplate.content.cloneNode(true);//Клонируем элемент с содержимым
+
+function render () {
+    const cards = initialCards.map(getCard);
+    elements.prepend(...cards);
+}
+function getCard(item) {
+    console.log(item);
+    elementPlace = elementTemplate.content.cloneNode(true);//Клонируем элемент с содержимым из шаблона
+    console.log(elementPlace);
+    const imagePlace = elementPlace.querySelector('.element__image');
+    imagePlace.src = item.link;
+    const namePlace = elementPlace.querySelector('.element__title');
+    namePlace.textContent = item.name;
+    const deleteButton = elementPlace.querySelector('.element__delete')
+    const likeButton = elementPlace.querySelector('.element__like');
+
+    deleteButton.addEventListener('click', deleteCard);
+    likeButton.addEventListener('click', likeCard)
+
+    return elementPlace;
+}
+function deleteCard (evt) {
+const card = evt.target.closest('.element');
+card.remove();
+}
+function likeCard (evt) {
+  const like = evt.target;
+  like.classList.toggle('element__like_active');
+}
+
+render();
+
+//......................... Код для попапа добавления карточки.........................
 
 let openAddButton = profile.querySelector('.profile__add-button');
-let addPopup = document.querySelector('#addPopup'); //Попап добавления картинки
+let addPopup = document.querySelector('#addPopup');//Попап добавления картинки
 let closeAddPopupButton = addPopup.querySelector('.popup__close');
-let addForm = addPopup.querySelector('.popup__form')
+let addForm = addPopup.querySelector('.popup__form');//Форма ПопАпа добавления карточки
 //.....Открыть попап......
 openAddButton.addEventListener('click', () => openPopup (addPopup));
 //.....Закрыть попап......
@@ -49,8 +113,15 @@ closeAddPopupButton.addEventListener('click', () => closePopup(addPopup));
 //.........Написать функцию работы попапа...........
 function formAddPlace (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    elementPlace = elementTemplate.content.cloneNode(true);//Клонируем элемент с содержимым
+    console.log(elementPlace);
+    let placeNameInput = addForm.querySelector('#placeNameInput').value;//Ввод названия места
+    let placeLinkInput = addForm.querySelector('#placeLinkInput').value;//Ввод ссылки на фото
+    const card = getCard({link: placeLinkInput, name: placeNameInput});
+    elements.prepend(card); 
     closePopup (addPopup);
 }
+
 addForm.addEventListener('submit', formAddPlace);
 
 //............................Код попапа с фотографией.................................................
