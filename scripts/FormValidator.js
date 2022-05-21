3762334453b03a8a7bf8fdc5b7216b621749ed1d
Gregori Orlov <Orlov.Gregori@yandex.ru>
2022-05-21 22:43:53 +0300

    настроить класс валидации

---
+++ scripts/FormValidator.js
@@ -0,0 +1,68 @@
+class FormValidator {
+    constructor (form, config) {
+        this._formElement = form,
+        this._config = config
+    }
+
+    disableSubmitButton (_buttonElement) {
+        _buttonElement.classList.add(this._config.buttonUnactive);
+        _buttonElement.setAttribute('disabled', true);
+    }
+
+    _showInputError = (_formElement, _inputElement, _errorMessage) => {
+        const _errorElement = _formElement.querySelector(`.${_inputElement.id}-error`);
+        _inputElement.classList.add(this._config.inputClassError);
+        _errorElement.textContent = _errorMessage;
+        _errorElement.classList.add(this._config.errorClass);
+      }
+      
+    _hideInputError = (_formElement, _inputElement) => {
+        const _errorElement = this._formElement.querySelector(`.${_inputElement.id}-error`);
+        _inputElement.classList.remove(this._config.inputClassError);
+        _errorElement.classList.remove(this._config.errorClass);
+        _errorElement.textContent = '';
+      }
+
+    _isValid = (_formElement, _inputElement) => {
+        if (!_inputElement.validity.valid) {
+          this._showInputError(_formElement, _inputElement, _inputElement.validationMessage);
+        } else {
+          this._hideInputError(_formElement, _inputElement);
+        }
+      }
+
+    _hasInvalidInput = (_inputList) => {
+        return _inputList.some((_inputElement) => {
+          return !_inputElement.validity.valid;
+        });
+       }
+
+    _toggleButtonState = (_inputList, _buttonElement) => {
+        if (this._hasInvalidInput(_inputList)) {
+          this.disableSubmitButton (_buttonElement);
+        } else {
+          _buttonElement.removeAttribute('disabled', true);
+          _buttonElement.classList.remove(config.buttonUnactive);
+        }
+      }
+
+    _setEventListeners (_formElement) {
+        const _inputList = Array.from(_formElement.querySelectorAll(this._config.input));
+        const _buttonElement = this._formElement.querySelector(this._config.submit);
+        this._toggleButtonState(_inputList, _buttonElement);
+        _inputList.forEach((_inputElement) => {
+          _inputElement.addEventListener('input', () => {
+          this._isValid(_formElement, _inputElement);
+        this._toggleButtonState(_inputList, _buttonElement);
+    });
+  });
+    }
+    
+    enableValidation () {
+        this._formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault()
+            });
+            this._setEventListeners (this._formElement);
+    }
+
+}
--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,8 @@ const job = profile.querySelector(".profile__desc");
 const formEdit = popupEdit.querySelector(".popup__form");
 const nameInput = formEdit.querySelector("#nameInput");
 const jobInput = formEdit.querySelector("#jobInput");
-const elements = document.querySelector(".elements"); //елемент не в единственном числе, это блок куда добавляются элементы, потому и назывл его
+const elements = document.querySelector(".elements"); //елемент не в единственном числе, это блок куда добавляются элементы, потому и назвал его
 //"Элементы". поnому что туда добавялются элементы из темплейта. Если я ошибаюсь, то какое имя ему нужно задать, чтобы не сбивать при этом с толку
-const elementTemplate = document.querySelector("#elementTemplate"); //**Найден в _getElement**Находим шаблон элемента
-//const photoPopup = document.querySelector("#photoPopup");
 const buttonClosePhotoPopup = photoPopup.querySelector(".popup__close");
 const buttonOpenAddPopup = profile.querySelector(".profile__add-button");
 const popupAdd = document.querySelector("#popupAdd"); //Попап добавления картинки
@@ -21,10 +19,27 @@ const placeNameInput = formAdd.querySelector("#placeNameInput"); //Ввод на
 const placeLinkInput = formAdd.querySelector("#placeLinkInput");
 
 const popups = document.querySelectorAll('.popup');
+
+
+const config = {
+  input: '.popup__input',
+  submit: '.popup__submit',
+  buttonUnactive: 'popup__submit_unactive',
+  inputClassError: 'popup__input_type_error',
+  errorClass: 'popup__input-error'
+}
+
+const cardForm = popupAdd.querySelector('.popup__form');
+const validateCardForm = new FormValidator(cardForm, config);
+validateCardForm.enableValidation();
+
+const editForm = popupEdit.querySelector('.popup__form');
+const validateEditForm = new FormValidator(editForm, config);
+validateEditForm.enableValidation();
+
 const setClosePopup = () => {
   popups.forEach(function (popup) {
     popup.addEventListener('click', (evt) => {
-      console.log(evt);
       if (evt.target.classList.contains('popup_opened')) {
         closePopup(popup);
       }
@@ -63,28 +78,7 @@ function editProfileInfo(evt) {
   closePopup(popupEdit);
 }
 
-/*
-function getCard(item) {
-  const elementPlace = elementTemplate.content.cloneNode(true); //Клонируем элемент с содержимым из шаблона
-  const imagePlace = elementPlace.querySelector(".element__image");
-  imagePlace.src = item.link;
-  const namePlace = elementPlace.querySelector(".element__title");
-  namePlace.textContent = item.name;
-  const buttonDelete = elementPlace.querySelector(".element__delete");
-  const buttonLike = elementPlace.querySelector(".element__like");
 
-  buttonDelete.addEventListener("click", deleteCard);
-  buttonLike.addEventListener("click", likeCard);
-  imagePlace.addEventListener("click", viewImage);
-
-  return elementPlace;
-}
-
-function render() {
-  const cards = initialCards.map(getCard);
-  elements.prepend(...cards);
-}
-*/
 function render() {
   initialCards.forEach((item) => {
     const card = new Card(item, '#elementTemplate');
@@ -95,8 +89,7 @@ function render() {
 
 
 function addPlaceForm(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. //Ввод ссылки на фото
-  //const card = getCard({ link: placeLinkInput.value, name: placeNameInput.value });
+  evt.preventDefault(); 
   const placeObj = {
     link: placeLinkInput.value,
     name: placeNameInput.value
@@ -106,26 +99,11 @@ function addPlaceForm(evt) {
   elements.prepend(card);
   closePopup(popupAdd);
   formAdd.reset();
-  disableSubmitButton (buttonSubmit);
+  buttonSubmit.classList.add('popup__submit_unactive');
+  buttonSubmit.setAttribute('disabled', true);
+  //disableSubmitButton (buttonSubmit);
 }
-/*
-function viewImage(evt) {
-  openPopup(photoPopup);
-  const card = evt.target.parentElement;
-  const popupImage = photoPopup.querySelector(".popup__image");
-  const popupTitle = photoPopup.querySelector(".popup__image-name");
-  popupImage.src = card.querySelector(".element__image").src;
-  popupTitle.textContent = card.querySelector(".element__title").textContent;
-}
-function deleteCard(evt) {
-  const card = evt.target.closest(".element");
-  card.remove();
-}
-function likeCard(evt) {
-  const like = evt.target;
-  like.classList.toggle("element__like_active");
-}
-*/
+
 render();
 
 formEdit.addEventListener("submit", editProfileInfo);
